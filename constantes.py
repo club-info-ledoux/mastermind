d7b83fd6555e368ecdeec1d45e06927106d96693
club-info-ledoux <151030950+club-info-ledoux@users.noreply.github.com>
2023-12-14 13:45:43 +0100

    Création des constantes

---
+++ constantes.py
@@ -0,0 +1,29 @@
+# Créé par clement.mathieu, le 14/12/2023 en Python 3.7
+
+#Couleurs que l'utilisateur peut proposer
+ROUGE=1
+ORANGE=2
+VERT=3
+BLEU=4
+ROSE=5
+JAUNE=6
+VIOLET=7
+BLEU=8
+
+#Couleurs que le programme utilise pour vérifier la proposition
+
+NOIR=11
+BLANC=12
+GRIS=13
+
+
+
+
+
+
+
+
+
+
+
+
